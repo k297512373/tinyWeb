94411edbceea6919299616a2b2378c89bd86444f
Nyh <konghuan1130@126.com>
2025-06-25 11:34:05 +0800

    页面修改，接口添加，注入方式修改

--- src/main/java/com/zh/nyh/config/MagicSaInterceptor.java
+++ src/main/java/com/zh/nyh/config/MagicSaInterceptor.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+//import org.ssssssss.magicapi.core.service.MagicResourceService;
 import org.ssssssss.magicapi.modules.db.SQLModule;
 
 import cn.dev33.satoken.fun.SaParamFunction;
@@ -23,59 +24,67 @@ import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 
 @Component
-public class MagicSaInterceptor extends SaInterceptor implements ApplicationListener<ContextRefreshedEvent>{
-	
+public class MagicSaInterceptor extends SaInterceptor implements ApplicationListener<ContextRefreshedEvent> {
+
 	@Autowired
 	private SQLModule db;
-	
+
+//	@Autowired
+//	private MagicResourceService magicResourceService;
+
 	private JSONArray pages;
-	
+
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		
-		pages =  new  JSONArray(db.camel().table("amis_page").where().eq("page_type", "3").select(null));
-		
+		refreshRouter();
+	}
+
+	public void refreshRouter() {
+
+		pages = new JSONArray(db.camel().table("amis_page").where().eq("page_type", "3").select(null));
+
 		auth = handler -> {
-			
+
 			List<String> notMatchs = new ArrayList<>();
-			notMatchs.addAll(ListUtil.of("/login","/login/captcha","/**/*.js","/**/*.css"));
-			
+			notMatchs.addAll(ListUtil.of("/login", "/login/captcha", "/**/*.js", "/**/*.css"));
+
 			Map<String, SaParamFunction<SaRouterStaff>> checks = new HashMap<>();
-			checks.put("/magic/**",r->StpUtil.checkRole("admin"));
-			
-			//初始化页面权限
-			for(Object o : pages) {
-				
-				//超级管理员可以访问所有页面
-				if(StpUtil.hasRole("admin")) {
+			checks.put("/magic/**", r -> StpUtil.checkRole("admin"));
+
+			// 初始化页面权限
+			for (Object o : pages) {
+
+				// 超级管理员可以访问所有页面
+				if (StpUtil.hasRole("admin")) {
 					break;
 				}
-				
+
 				JSONObject page = new JSONObject(o);
 				JSONObject power = page.getJSONObject("pagePower");
-				
-				if(power != null && StrUtil.equals("any", power.getStr("power"))) {
-					notMatchs.add("/amis/view/"+page.getStr("id"));
+
+				if (power != null && StrUtil.equals("any", power.getStr("power"))) {
+					notMatchs.add("/amis/view/" + page.getStr("id"));
 					continue;
-				}else if(power==null) {
+				} else if (power == null) {
 					continue;
 				}
-				
+
 				JSONArray roles = power.getJSONArray("roles");
-				
-				if(roles!=null&&!roles.isEmpty()) {
-					checks.put("/amis/view/"+page.getStr("id"),r->StpUtil.checkRoleOr(roles.toArray(new String[0])));
+
+				if (roles != null && !roles.isEmpty()) {
+					checks.put("/amis/view/" + page.getStr("id"),
+							r -> StpUtil.checkRoleOr(roles.toArray(new String[0])));
 				}
-				
+
 			}
-			
-			SaRouter.match("/**").notMatch(notMatchs).check(r->StpUtil.checkLogin());
-			
-			for(Entry<String, SaParamFunction<SaRouterStaff>> check : checks.entrySet()) {
-				SaRouter.match(check.getKey(),check.getValue());
+
+			SaRouter.match("/**").notMatch(notMatchs).check(r -> StpUtil.checkLogin());
+
+			for (Entry<String, SaParamFunction<SaRouterStaff>> check : checks.entrySet()) {
+				SaRouter.match(check.getKey(), check.getValue());
 			}
-			
-        };
+
+		};
+
 	}
-	
 }
